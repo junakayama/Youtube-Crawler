708157cc30388ac943e0a0b36ca5fd5ae1850d49
Júlia Nakayama Silveira <junakayama97@gmail.com>
2018-04-22 15:31:53 -0300

    writing in a csv file

--- yt_crawler.py
+++ yt_crawler.py
@@ -7,9 +7,22 @@ browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 url = 'https://www.youtube.com/results?search_query=chitaozinho+e+xororo'
 browser.get(url)
 
+filename = "chitaozinhoexororo.csv"
+f = open(filename, "w")
+
+headers = "title, views\n"
+f.write(headers)
+
 titles = browser.find_elements(By.CSS_SELECTOR, '#contents > ytd-video-renderer > #dismissable > div > #meta > #title-wrapper > h3 > #video-title')
-num_titles = len(titles)
-for i in range(num_titles):
-	print(titles[i].text)
+views = browser.find_elements(By.CSS_SELECTOR, '#contents > ytd-video-renderer > #dismissable > div > #meta > ytd-video-meta-block > #metadata > #metadata-line > span:nth-child(1)')
+
+num_videos = len(titles)
+
+for i in range(num_videos):
+	print(titles[i].text +': '+views[i].text)
+
+	f.write(titles[i].text + "," + views[i].text + "\n")
 
 browser.close()
+f.close()
+
